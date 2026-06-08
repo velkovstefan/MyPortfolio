f882c69bf7d43f8d36c1d7dee0151d26a37abd8c
Stefan Velkov <velkov29stefan@gmail.com>
2026-06-08 19:49:13 +0200

    conf changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/MyPortfolio/",
   plugins: [react(), tailwindcss()],
   server: {
     host: true, // exposes on 0.0.0.0
